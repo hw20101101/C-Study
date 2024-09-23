7f533fc69fdee6a1cf39fd87e7e784a64500d073
hwacdx <1296324895@qq.com>
2024-09-23 16:22:29 +0800

    运算符

--- H240923-C/H240923-C/main.c
+++ H240923-C/H240923-C/main.c
@@ -13,7 +13,7 @@
 extern int x, y;
 
 //定义全局变量
-int a, b;
+int a1, b1;
 
 void test1(void){
     
@@ -54,6 +54,38 @@ void test2(void){
 
 // -------------------- C 常量 END --------------------
 
+void test3(void){
+    
+    //位运算符
+    unsigned int aa = 60; // 60 = 0011 1100
+    unsigned int bb = 13; // 13 = 0000 1101
+    int cc = 0;
+    
+    cc = aa & bb; // 12 = 0000 1100
+    printf("cc value:%d \n", cc);
+    
+    cc = aa | bb; // 61 = 0011 1101
+    printf("2 cc value:%d \n", cc);
+    
+    cc = aa ^ bb; //49 = 0011 0001
+    printf("3 cc value:%d \n", cc);
+    
+    
+    // 杂项运算符
+    int a = 4;
+    short b;
+    double c;
+    int *ptr;
+    
+    printf("a size:%lu \n", sizeof(a));
+    printf("b size:%lu \n", sizeof(b));
+    printf("c size:%lu \n", sizeof(c));
+    
+    ptr = &a; //ptr 包含 a 的地址
+    printf("a value:%d \n", a);
+    printf("*ptr:%d \n", *ptr);
+}
+
 int count22;
 extern void write_extern(void);
 
@@ -68,8 +100,10 @@ int main(int argc, const char * argv[]) {
         test2();
     } */
     
-    count22 = 5;
-    write_extern();
+    //count22 = 5;
+    //write_extern();
+    
+    test3();
     
     return 0;
 }

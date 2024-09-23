47f9a21ef3acb46fa18f7c762eff0569585140de
hwacdx <1296324895@qq.com>
2024-09-23 15:35:03 +0800

    create project

---
+++ H240923-C/H240923-C/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  H240923-C
+//
+//  Created by hwacdx on 2024/9/23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
